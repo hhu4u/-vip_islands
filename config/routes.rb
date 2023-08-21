2bb6547070e26b36faa455f475b453026b2c870b
herve hublet <hervehublet@hotmail.com>
2023-08-21 17:58:26 +0200

    Implemented feedback from Marianna

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+  root to: "islands#index"
   devise_for :users
   # root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root to: "islands#index"
 end
--- db/migrate/20230821153730_devise_create_users.rb
+++ db/migrate/20230821153730_devise_create_users.rb
@@ -34,8 +34,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
 
       t.string :first_name
       t.string :last_name
-      t.string :email
-      t.string :password
 
       t.timestamps null: false
     end
