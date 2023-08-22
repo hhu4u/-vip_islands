e3b95964aab4351b942c920e0752f2e4e9ddf011
Marianna Rekoumi <marianna_rek@hotmail.com>
2023-08-22 16:20:02 +0200

    Add dotenv-rails

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "open-uri"
+
+user = User.new(first_name: "Test", last_name: "Test", email: "test@user.com", password: "123456")
+
+
+file = URI.open("")
+island = Island.new(name: "Beautiful island", description: "", size: 150, number_of_guests: 5, price_per_night: 8000)
+island.user = user
+island.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
+island.save!
