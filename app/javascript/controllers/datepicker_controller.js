23b9410b5e37522fc7b739eac34b5a5e97b55446
Marianna Rekoumi <marianna_rek@hotmail.com>
2023-08-24 16:44:23 +0200

    clearing logs in datepicker controller

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -4,10 +4,8 @@ import flatpickr from "flatpickr";
 // Connects to data-controller="datepicker"
 export default class extends Controller {
   connect() {
-    console.log("hello")
-    console.log(this.startTarget)
     flatpickr(this.element)
-
-
   }
+
+
 }
